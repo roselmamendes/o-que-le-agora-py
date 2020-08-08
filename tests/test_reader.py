3b8a8e3eb845e2b6d65c44782a2c784cdff79680
Roselma <roselma.mendes@gmail.com>
2020-08-07 21:28:19 -0300

    feed.reader retorna um json

--- tests/test_reader.py
+++ tests/test_reader.py
@@ -2,16 +2,30 @@ import unittest
 from unittest.mock import Mock
 from feed.reader import get_rss_feed_for
 
-class FeedObj:
-    def __init__(self, title):
-        self.title = title
-
+# ['title', 'title_detail', 'links', 'link', 'id', 'guidislink', 'authors', 'author', 'author_detail', 'published', 'published_parsed', 'updated', 'updated_parsed', 'content', 'summary']
 class TestReader(unittest.TestCase):
     def test_get_a_feed(self):
         feed_parser = Mock()
-        feed_parser.parse.return_value = FeedObj('titulo')
+        feed_parser.parse.return_value = {'feed': {'title': 'titulo'}}
         
         rss_feed = get_rss_feed_for('http://site.com', feed_parser)
 
         feed_parser.parse.assert_called_with('http://site.com')
-        self.assertEqual(rss_feed.title, 'titulo')
+        self.assertEqual(rss_feed['feed']['title'], 'titulo')
+
+    def test_get_an_entry(self):
+        feed_parser = Mock()
+        feed_parser.parse.return_value = {
+        'entries':
+            [ 
+                {
+                    'title': 'titulo',
+                    'content': '<div>content</div>'
+                }
+            ]}
+
+        rss_feed = get_rss_feed_for('http://site.com', feed_parser)
+
+        feed_parser.parse.assert_called_with('http://site.com')
+        self.assertEqual(rss_feed['entries'][0]['title'], 'titulo')
+        self.assertEqual(rss_feed['entries'][0]['content'], '<div>content</div>')
