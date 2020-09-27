e35a4aa7ebc913513f9256d50245629babc27733
Roselma <roselma.mendes@gmail.com>
2020-09-27 08:45:56 -0300

    retorna lista de rss identifiers

--- app.py
+++ app.py
@@ -10,16 +10,21 @@ def get_feed_posts(rss_identifier):
     print(f'app.py - 200 - get_posts - For {rss_identifier} got {len(posts)} posts')
     return jsonify(posts)
 
-@app.route('/rss', methods=['POST'])
-def add_site_feed():
-    payload = request.get_json()
-    result = rss_use_case.save_rss_url(payload)
-    if (not result):
-        print(f'app.py - 201 - add_site_feed - Added {payload["rss_identifier"]}')
-        return {}, 201
-    else:
-        print(f'app.py - 400 - add_site_feed - {result}')
-        return {'error': result}, 400
+@app.route('/rss', methods=['POST', 'GET'])
+def rss():
+    if request.method == 'POST':
+        payload = request.get_json()
+        result = rss_use_case.save_rss_url(payload)
+        if (not result):
+            print(f'app.py - 201 - add_site_feed - Added {payload["rss_identifier"]}')
+            return {}, 201
+        else:
+            print(f'app.py - 400 - add_site_feed - {result}')
+            return {'error': result}, 400
+    if request.method == 'GET':
+        result = rss_use_case.get_all_rss_identifier()
+        print(f'app.py - 200 - /rss -  Got {len(result)} rss identifiers')
+        return jsonify(result)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
